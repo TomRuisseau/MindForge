37999170b312e7c1e004e3217e407b6b275ca115
TomRuisseau <tom.ruisseau@student.junia.com>
2023-05-24 13:05:58 +0200

    Base de donnée

--- js/back/index.js
+++ js/back/index.js
@@ -9,3 +9,29 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const mysql = require ('mysql');
+
+const pool = mysql.createPool({
+  host : '89.116.147.1',
+  port : '3306',
+  user : 'u495496740_Moi',
+  password : 'N0v/EGU:',
+  database : 'u495496740_Propro', 
+})
+
+
+pool.getConnection(function(err, connection) {
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  //connection.query("SELECT * FROM Sorts", function (err, result, fields) {
+    //if (err) throw err;
+    //console.log(result);
+  //}); 
+});
+
