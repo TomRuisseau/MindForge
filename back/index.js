a310d8652c795832aafafcaf3ed941bfba355517
TomRuisseau <tom.ruisseau@student.junia.com>
2023-05-24 14:27:36 +0200

    app set up avec react et node

--- back/index.js
+++ back/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+  console.log("allo")
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
