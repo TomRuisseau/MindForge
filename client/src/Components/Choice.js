a29cb0b3037d1175e5d0f944b3316e1f4c7e8d1b
TomRuisseau <tom.ruisseau@student.junia.com>
2023-05-25 16:46:05 +0200

    Clean files + bootstrap

---
+++ client/src/Components/Choice.js
@@ -0,0 +1,13 @@
+function Choice() {
+    return (
+        <>
+            <h1>Vous êtes :</h1>
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-primary btn-lg">Un enseignant</button>
+                <button type="button" class="btn btn-primary btn-lg">Un élève</button>
+            </div>
+        </>
+    )
+}
+
+export default Choice;
