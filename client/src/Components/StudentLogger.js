5f985f0af01ac53f533b1b947e62b856febcc578
TomRuisseau <tom.ruisseau@student.junia.com>
2023-05-25 18:50:45 +0200

    login pour étudiant

--- client/src/Components/StudentLogger.js
+++ client/src/Components/StudentLogger.js
@@ -1,7 +1,20 @@
+import React, { useState } from "react";
+
 function StudentLogger() {
+    const [code, setCode] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevent page reload
+        console.log(code);
+    }
+
     return (
         <>
-            <h1>Student logger</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="code">Entre le code fourni par ton enseignant</label>
+                <input value={code} onChange={(e) => setCode(e.target.value)} type="text" className="form-control" placeholder="xyz123" id="code" name="code" />
+                <button type="submit" className="btn btn-primary">Connexion</button>
+            </form>
         </>
     )
 }
