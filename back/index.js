f9bfd2f061dd8965a991fbbbcdad348cab0b05e8
TomRuisseau <tom.ruisseau@student.junia.com>
2023-05-27 16:51:13 +0200

    Test de communication client/server

--- back/index.js
+++ back/index.js
@@ -1,35 +1,42 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 5000;
+var cors = require('cors')
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(cors()) //Prevent CORS errors
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.post("/api", (req, res) => {
+  console.log(req.body.message);
+  res.send("Hello from server!");
 });
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
 
-const mysql = require ('mysql');
+const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  host : '89.116.147.1',
-  port : '3306',
-  user : 'u495496740_Moi',
-  password : 'N0v/EGU:',
-  database : 'u495496740_Propro', 
+  host: '89.116.147.1',
+  port: '3306',
+  user: 'u495496740_Moi',
+  password: 'N0v/EGU:',
+  database: 'u495496740_Propro',
 })
 
 
-pool.getConnection(function(err, connection) {
+pool.getConnection(function (err, connection) {
   //connection.query("SELECT * FROM Sorts", function (err, result, fields) {
-    //if (err) throw err;
-    //console.log(result);
+  //if (err) throw err;
+  //console.log(result);
   //}); 
 });
 
--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import StrudentLogger from './Components/StudentLogger';
 import React, { useState } from 'react';
 import StudentDashboard from './Components/StudentDashboard';
 import TeacherDashboard from './Components/TeacherDashboard';
+import axios from 'axios';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Choice'); // ['Choice', 'TeacherLogger', 'StudentLogger', 'StudentDashboard', 'TeacherDashboard']
@@ -11,17 +12,30 @@ function App() {
   const switchPage = (page) => {
     setCurrentPage(page);
   }
-  return (
-    <div className="App">
-      {
-        currentPage === "Choice" ? <Choice onChoice={switchPage} /> :
-          currentPage === "TeacherLogger" ? <TeacherLogger onValidation={switchPage} /> :
-            currentPage === "StudentLogger" ? <StrudentLogger onValidation={switchPage} /> :
-              currentPage === "StudentDashboard" ? <StudentDashboard /> :
-                <TeacherDashboard />
 
-      }
-    </div>
+  const onTest = () => {
+    axios.post('http://localhost:5000/api', { message: "Hello from client" })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  return (
+    <>
+      <button onClick={() => onTest()}>Test server</button>
+      <div className="App">
+        {
+          currentPage === "Choice" ? <Choice onChoice={switchPage} /> :
+            currentPage === "TeacherLogger" ? <TeacherLogger onValidation={switchPage} /> :
+              currentPage === "StudentLogger" ? <StrudentLogger onValidation={switchPage} /> :
+                currentPage === "StudentDashboard" ? <StudentDashboard /> :
+                  <TeacherDashboard />
+
+        }
+      </div>
+    </>
   );
 }
 
