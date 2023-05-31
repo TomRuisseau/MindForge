7d21b0f94b6e8bdfc481bf913d55a0e1d13f18f5
Xavier LEDUC <xavier.leduc@student.junia.com>
2023-05-31 15:16:29 +0200

    1516

--- client/src/Components/Shop.js
+++ client/src/Components/Shop.js
@@ -7,6 +7,22 @@ function Shop() {
                 <div className='col-2'></div>
                 <h1 className='col'>Boutique</h1>
             </div>
+            <br></br>
+            <div className='row'>
+                <div className='col-2'></div>
+                <div className='col-2'>
+                    <h3>Item 1</h3>
+                </div>
+                <div className='col-2'>
+                    <h3>Item 2</h3>
+                </div>
+                <div className='col-2'>
+                    <h3>Item 3</h3>
+                </div>
+                <div className='col-2'>
+                    <h3>Item 4</h3>
+                </div>
+            </div>
         
         
         
