5887b5f08ea6fb9d3febedad3335c1e36ca21e9c
Xavier LEDUC <xavier.leduc@student.junia.com>
2023-05-31 13:02:08 +0200

    1302

--- client/src/Components/StudentDashboard.js
+++ client/src/Components/StudentDashboard.js
@@ -6,6 +6,17 @@ function StudentDashboard() {
         <div>
             <StudentDrawer />
             <h1 className="text-center">Student Dashboard</h1>
+            <div className="row m-0">
+                <div className="col-2">
+                </div>
+                <div className="col-3  m-0 h-500 bg-success">
+                    <h2>Ton nom:</h2>
+                    <h2>Ta classe: </h2>
+                </div>
+                <div className="col m-0 h-500 bg-danger">
+                    <h2>Ton personnage:</h2>
+                </div>
+            </div>
         </div>
     )
 }
