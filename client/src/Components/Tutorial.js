78f2b762b5025bf880e86af6f225bc300453eb00
Xavier LEDUC <xavier.leduc@student.junia.com>
2023-05-31 16:20:21 +0200

    Tutoriel fini avec du lorem ipsum

--- client/src/Components/Tutorial.js
+++ client/src/Components/Tutorial.js
@@ -13,12 +13,17 @@ function Tutorial(){
                 <div className='col-3'>
                     <h2>Comment jouer:</h2>
                     <p className="text-justify">Le but du jeu est de répondre correctement aux questions posées par le professeur. Pour cela, il faut cliquer sur la bonne réponse parmi les propositions. Si la réponse est correcte, le joueur gagne des points. Si la réponse est incorrecte, le joueur perd des points. Le joueur peut également gagner des points en réalisant des quêtes.</p>
-                <div className='col-2'>
+                </div>
+                <div className='col-3'>
+                    <h2>Comment gagner:</h2>
+                    <p className="text-justify">Le joueur gagne la partie lorsqu'il a atteint le nombre de points requis pour gagner. Le joueur peut également gagner la partie en réalisant des quêtes.</p>
+                </div>
+                <div className='col-3'>
                     <h2>Comment gagner:</h2>
                     <p className="text-justify">Le joueur gagne la partie lorsqu'il a atteint le nombre de points requis pour gagner. Le joueur peut également gagner la partie en réalisant des quêtes.</p>
                 </div>
                     
-                </div>
+                
             </div>
         </>
     )
