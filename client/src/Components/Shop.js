d62a87222bac2aa0adbe54625155cc601444458e
TomRuisseau <tom.ruisseau@student.junia.com>
2023-06-06 15:32:06 +0200

    selection dans la boutique

--- client/src/Components/Shop.js
+++ client/src/Components/Shop.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../Styles/boutique.css"
 
 function Shop(props) {
     const [spells, setSpells] = useState([]); // liste des sorts
     const [skins, setSkins] = useState([]); // liste des skins
+    const [selected, setSelected] = useState(""); // liste des skins
 
     useEffect(() => {
         axios.post("http://localhost:5000/getSpellsShop", { id: props.data[0].id, class: props.data[0].class })
@@ -24,6 +26,17 @@ function Shop(props) {
             );
     }, [props.data]);
 
+    const select = (e) => {
+        if (e.target.getAttribute("data-key") === null) {
+            setSelected(e.target.parentNode.getAttribute("data-key"));
+            console.log(e.target.parentNode.getAttribute("data-key"));
+        }
+        else {
+            setSelected(e.target.getAttribute("data-key"));
+            console.log(e.target.getAttribute("data-key"));
+        }
+    }
+
     return (
         <>
             <div className='row'>
@@ -33,25 +46,31 @@ function Shop(props) {
             </div>
             <br></br>
             <div className='row'>
-                <div className='col p-5 overflow-auto'>
+                <div className='col p-5'>
                     <h2>Sorts</h2>
-                    {spells.map((spell) => {
-                        return (
-                            <div key={spell.name}>
-                                <h2>{spell.name.split("_").join(" ")}</h2>
-                                <img src={`media/spells/${spell.name}.webp`}></img>
-                                <p>{!spell.owned ? "Prix : " + spell.cost + " XP" : "Déjà possédé"}</p>
-                            </div>
-                        )
-                    }
-                    )}
+                    <div className='d-flex flex-row'>
+                        {spells.map((spell) => {
+                            let bg = spell.name === selected ? "selected" : "";
+                            let className = "m-2 " + bg;//mettre ici les autres classes de la div
+                            return (
+                                <div key={spell.name} data-key={spell.name} className={className} onClick={select}>
+                                    <h2>{spell.name.split("_").join(" ")}</h2>
+                                    <img src={`media/spells/${spell.name}.webp`}></img>
+                                    <p>{!spell.owned ? "Prix : " + spell.cost + " XP" : "Déjà possédé"}</p>
+                                </div>
+                            )
+                        }
+                        )}
+                    </div>
                 </div>
                 <div className="col">
                     <h2>Skins</h2>
                     <div className='d-flex flex-row'>
                         {skins.map((skin) => {
+                            let bg = skin.name === selected ? "selected" : "";
+                            let className = "m-2 " + bg;//mettre ici les autres classes de la div
                             return (
-                                <div key={skin.name}>
+                                <div key={skin.name} data-key={skin.name} className={className} onClick={select}>
                                     <h2>{skin.name.split("_").join(" ")}</h2>
                                     <img src={`media/skin/${skin.name}.png`} style={{ width: "100%" }}></img>
                                     <p>{!skin.owned ? "Prix : " + skin.cost + " XP" : "Déjà possédé"}</p>
