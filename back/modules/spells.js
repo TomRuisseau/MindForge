8155abbc88ef1bebf9aeba818e2d10cf9475d08b
TomRuisseau <tom.ruisseau@student.junia.com>
2023-06-07 11:30:15 +0200

    sorts

--- back/modules/spells.js
+++ back/modules/spells.js
@@ -5,7 +5,7 @@ exports.SpellsCosts.set("imposition_des_mains", 4);
 exports.SpellsCosts.set("halo_salvateur", 6);
 exports.SpellsCosts.set("purification", 3);
 exports.SpellsCosts.set("apaisement_majeur", 6);
-exports.SpellsCosts.set("premier_soin", 2);
+exports.SpellsCosts.set("premiers_soins", 2);
 exports.SpellsCosts.set("reviviscence", 6);
 exports.SpellsCosts.set("soin_de_masse", 6);
 exports.SpellsCosts.set("vague_de_mana", 6);
