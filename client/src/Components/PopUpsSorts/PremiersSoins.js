f7519e92abc33c0bc19ae1ecfb4b5f6b57a43281
MarieDevulder <marie.devulder@student.junia.com>
2023-06-07 16:14:29 +0200

    Update PremiersSoins.js

--- client/src/Components/PopUpsSorts/PremiersSoins.js
+++ client/src/Components/PopUpsSorts/PremiersSoins.js
@@ -1,6 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import e from "cors";
+
 function PremiersSoins(props) {
-  const useSpell = () => {
-    console.log("Premiers soins");
+  const [students, setStudents] = useState([]);
+  const [student, setStudent] = useState("0");
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/getStudentsTeam", {
+        team: props.data[0].team,
+      })
+      .then((res) => {
+        setStudents(res.data);
+        setStudent(res.data[0].id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [props.data]);
+
+  const useSpell = (e) => {
+    e.preventDefault(); // prevent page reload
+    console.log(student);
   };
 
   return (
@@ -16,10 +39,34 @@ function PremiersSoins(props) {
         </p>
       </div>
       <h3>Coût en mana : 2</h3>
-      <div>
-        <button onClick={useSpell} className="btn btn-success btn-lg">
-          Utiliser
-        </button>
+      <div className="w-50">
+        <form
+          onSubmit={useSpell}
+          className="d-flex flex-row justify-content-between"
+        >
+          <div className="d-flex flex-column">
+            <label htmlFor="text" className="mt-3">
+              Choisir un membre de l'équipe sur qui utiliser le sort :
+            </label>
+            <select
+              name="student"
+              id="student-select"
+              className="rounded"
+              onChange={(e) => setStudent(e.target.value)}
+            >
+              {students.map((eleve) => {
+                return (
+                  <option value={eleve.id} key={eleve.id}>
+                    {eleve.first_name} {eleve.surname}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <button type="submit" className="btn btn-success btn-lg">
+            Utiliser
+          </button>
+        </form>
       </div>
     </div>
   );
