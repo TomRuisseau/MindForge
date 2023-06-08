6791bf9ada98c6ba03dbc1fb25171c1fc99089f4
MarieDevulder <marie.devulder@student.junia.com>
2023-06-08 11:08:34 +0200

    Update Reviviscence.js

--- client/src/Components/PopUpsSorts/Reviviscence.js
+++ client/src/Components/PopUpsSorts/Reviviscence.js
@@ -1,7 +1,18 @@
-function Reviviscence() {
+function Reviviscence(props) {
   return (
-    <div>
-      <h1>Reviviscence</h1>
+    <div className="px-3 py-3 w-50 h-50 bg-light position-absolute top-50 start-50 translate-middle text-center border rounded border-success d-flex flex-column align-items-center justify-content-between">
+      <div className="d-flex flex-row justify-content-between">
+        <h1 className="px-5">Reviviscence</h1>
+        <button className="btn-close h-auto" onClick={props.close}></button>
+      </div>
+      <div className="p-3 rounded">
+        <p style={{ fontSize: "22px" }}>
+          Permet à un membre qui n'a plus de vie de revenir à{" "}
+          <span className="text-danger">1 HP</span> sans subir le malus
+          aléatoire.
+        </p>
+      </div>
+      <h3>Coût en mana : 6</h3>
     </div>
   );
 }
