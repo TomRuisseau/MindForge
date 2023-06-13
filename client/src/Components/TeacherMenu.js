3503a1848a79181d7b7332dc045b74cd840c9d33
TomRuisseau <tom.ruisseau@student.junia.com>
2023-06-13 16:19:10 +0200

    horloge trop belle

--- client/src/Components/TeacherMenu.js
+++ client/src/Components/TeacherMenu.js
@@ -1,7 +1,21 @@
-const TeacherMenu = () => {
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import 'react-clock/dist/Clock.css';
+
+function TeacherMenu() {
+    const [value, setValue] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setValue(new Date()), 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
     return (
-        <div className="h-100">
-            <h1 className="text-center">Teacher Menu</h1>
+        <div className='bg-white rounded-circle m-5' style={{ width: "500px" }}>
+            <Clock value={value} size={500} />
         </div>
     );
 }
