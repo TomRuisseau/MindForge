de3765f1aded52d10656dae8d7ebfa51fd528460
TomRuisseau <tom.ruisseau@student.junia.com>
2023-06-14 16:17:51 +0200

    boutiques dans un fichier

--- back/index.js
+++ back/index.js
@@ -13,6 +13,7 @@ const { setPostStudentsGetters } = require("./routes/studentsGetters.js")
 const { setPostInfosGetters } = require("./routes/infosGetter.js");
 const { setPostQuests } = require("./routes/questsManager.js");
 const { setPostActions } = require("./routes/actions.js");
+const { setPostBoutique } = require("./routes/boutique.js");
 
 addXp = functions.addXp;
 addXpWithoutSend = functions.addXpWithoutSend;
@@ -49,120 +50,13 @@ setPostStudentsGetters(app, pool);
 setPostInfosGetters(app, pool, classMap, SpellsCosts);
 setPostQuests(app, pool, addXp);
 setPostActions(app, pool, classMap, addXp, addMana);
+setPostBoutique(app, pool);
 
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-app.post("/getSkinsShop", (req, res) => {
-  pool.getConnection(function (err, connection) {
-    console.log("début boutique")
-    let respons = { skins: [], spells: [] }
-    connection.query(
-      "SELECT * FROM item WHERE type = 'skin'",
-      function (err, allSkins, fields) {
-        if (err) throw err;
-        connection.query(
-          "SELECT item_name, equiped FROM owned_item WHERE student_id = '"
-          + req.body.id
-          + "'",
-          function (err, result, fields) {
-            allSkins.forEach((spell) => {
-              spell.owned = false;
-              result.forEach((ownedSpell) => {
-                if (spell.name === ownedSpell.item_name) {
-                  spell.owned = true;
-                  spell.equiped = ownedSpell.equiped;
-                }
-              });
-            }
-            );
-            respons.skins = allSkins;
-            connection.query(
-              "SELECT * FROM item WHERE type = 'spell_" + req.body.class + "'",
-              function (err, allSpells, fields) {
-                if (err) throw err;
-                connection.query(
-                  "SELECT item_name FROM owned_item WHERE student_id = '"
-                  + req.body.id
-                  + "'",
-                  function (err, result, fields) {
-                    allSpells.forEach((spell) => {
-                      spell.owned = false;
-                      result.forEach((ownedSpell) => {
-                        if (spell.name === ownedSpell.item_name) {
-                          spell.owned = true;
-                        }
-                      });
-                    }
-                    );
-                    respons.spells = allSpells;
-                    console.log("fin boutique")
-                    res.send(respons);
-                  }
-                );
-              });
-          }
-        );
-      });
-  });
-});
-
-//buy a spell or item 
-app.post("/buy", (req, res) => {
-  //receive id and item name
-  pool.getConnection(function (err, connection) {
-    connection.query(
-      "INSERT INTO owned_item(student_id, item_name, equiped) VALUES ('" +
-      req.body.id +
-      "', '" +
-      req.body.name +
-      "', '0')",
-      function (err, result, fields) {
-        if (err) throw err;
-        connection.query(
-          "UPDATE student SET xp = '" +
-          req.body.newXp +
-          "' WHERE id = '" +
-          req.body.id +
-          "'",
-          function (err, result, fields) {
-            if (err) throw err;
-            res.send("0");
-          }
-        );
-      }
-    );
-  });
-});
-
-//equip an item
-app.post("/equip", (req, res) => {
-  //receive id and item name
-  pool.getConnection(function (err, connection) {
-    connection.query(
-      "UPDATE owned_item SET equiped = '0' WHERE student_id = '" +
-      req.body.id +
-      "'",
-      function (err, result, fields) {
-        if (err) throw err;
-        connection.query(
-          "UPDATE owned_item SET equiped = '1' WHERE student_id = '" +
-          req.body.id +
-          "' AND item_name = '" +
-          req.body.name +
-          "'",
-          function (err, result, fields) {
-            if (err) throw err;
-            res.send("0");
-          }
-        );
-      }
-    );
-  });
-});
 
 //get tanks that could protect 
 app.post("/getTanks", (req, res) => {
---
+++ back/routes/boutique.js
@@ -0,0 +1,111 @@
+
+exports.setPostBoutique = function setPostBoutique(app, pool) {
+
+    app.post("/getSkinsShop", (req, res) => {
+        pool.getConnection(function (err, connection) {
+            console.log("début boutique")
+            let respons = { skins: [], spells: [] }
+            connection.query(
+                "SELECT * FROM item WHERE type = 'skin'",
+                function (err, allSkins, fields) {
+                    if (err) throw err;
+                    connection.query(
+                        "SELECT item_name, equiped FROM owned_item WHERE student_id = '"
+                        + req.body.id
+                        + "'",
+                        function (err, result, fields) {
+                            allSkins.forEach((spell) => {
+                                spell.owned = false;
+                                result.forEach((ownedSpell) => {
+                                    if (spell.name === ownedSpell.item_name) {
+                                        spell.owned = true;
+                                        spell.equiped = ownedSpell.equiped;
+                                    }
+                                });
+                            }
+                            );
+                            respons.skins = allSkins;
+                            connection.query(
+                                "SELECT * FROM item WHERE type = 'spell_" + req.body.class + "'",
+                                function (err, allSpells, fields) {
+                                    if (err) throw err;
+                                    connection.query(
+                                        "SELECT item_name FROM owned_item WHERE student_id = '"
+                                        + req.body.id
+                                        + "'",
+                                        function (err, result, fields) {
+                                            allSpells.forEach((spell) => {
+                                                spell.owned = false;
+                                                result.forEach((ownedSpell) => {
+                                                    if (spell.name === ownedSpell.item_name) {
+                                                        spell.owned = true;
+                                                    }
+                                                });
+                                            }
+                                            );
+                                            respons.spells = allSpells;
+                                            console.log("fin boutique")
+                                            res.send(respons);
+                                        }
+                                    );
+                                });
+                        }
+                    );
+                });
+        });
+    });
+
+    //buy a spell or item 
+    app.post("/buy", (req, res) => {
+        //receive id and item name
+        pool.getConnection(function (err, connection) {
+            connection.query(
+                "INSERT INTO owned_item(student_id, item_name, equiped) VALUES ('" +
+                req.body.id +
+                "', '" +
+                req.body.name +
+                "', '0')",
+                function (err, result, fields) {
+                    if (err) throw err;
+                    connection.query(
+                        "UPDATE student SET xp = '" +
+                        req.body.newXp +
+                        "' WHERE id = '" +
+                        req.body.id +
+                        "'",
+                        function (err, result, fields) {
+                            if (err) throw err;
+                            res.send("0");
+                        }
+                    );
+                }
+            );
+        });
+    });
+
+    //equip an item
+    app.post("/equip", (req, res) => {
+        //receive id and item name
+        pool.getConnection(function (err, connection) {
+            connection.query(
+                "UPDATE owned_item SET equiped = '0' WHERE student_id = '" +
+                req.body.id +
+                "'",
+                function (err, result, fields) {
+                    if (err) throw err;
+                    connection.query(
+                        "UPDATE owned_item SET equiped = '1' WHERE student_id = '" +
+                        req.body.id +
+                        "' AND item_name = '" +
+                        req.body.name +
+                        "'",
+                        function (err, result, fields) {
+                            if (err) throw err;
+                            res.send("0");
+                        }
+                    );
+                }
+            );
+        });
+    });
+};
