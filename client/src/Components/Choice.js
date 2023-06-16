94b97a133dd897bd54b720ddf74db3dd63cd7fcb
Clara-gille <clara.gille@student.junia.com>
2023-06-16 14:36:23 +0200

    Page d'accueil modif

--- client/src/Components/Choice.js
+++ client/src/Components/Choice.js
@@ -41,6 +41,7 @@ function Choice(props) {
                 <div className="navbar">
                     <h1> Se connecter : </h1>
                     <h1> enseignant </h1>
+                    <h1> | </h1>
                     <h1> élève </h1>
                 </div>
                 
@@ -100,8 +101,8 @@ function Choice(props) {
                     <div className="mainChoice">
                         <h1>Vous êtes :</h1>
                         <div className="btn-group" role="group" aria-label="Basic example">
-                            <button onClick={() => props.onChoice('TeacherLogger')} type="button" className="btn btn-primary btn-lg">Un enseignant</button>
-                            <button onClick={() => props.onChoice('StudentLogger')} type="button" className="btn btn-primary btn-lg">Un élève</button>
+                            <button onClick={() => props.onChoice('TeacherLogger')} type="button" className="btn btn-primary btn-lg">Enseignant</button>
+                            <button onClick={() => props.onChoice('StudentLogger')} type="button" className="btn btn-primary btn-lg">Elève</button>
                         </div>
                     </div>
             </div>
