95f68df72dfa4077f0e6d66c92a9eb18a97eaa52
MarieDevulder <marie.devulder@student.junia.com>
2023-06-18 15:12:11 +0200

    tiroir eleves

--- client/src/Components/StudentDrawer.js
+++ client/src/Components/StudentDrawer.js
@@ -1,49 +1,126 @@
-import React, { forwardRef, useImperativeHandle } from 'react';
-import '../Styles/studentDrawer.css'
-
-// import component
-import Drawer from 'react-modern-drawer'
-
-//import styles
-import 'react-modern-drawer/dist/index.css'
+import React, { forwardRef, useImperativeHandle } from "react";
+import "../Styles/studentDrawer.css";
+import { motion } from "framer-motion";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 const StudentDrawer = forwardRef((props, ref) => {
-    const [isOpen, setIsOpen] = React.useState(false)
-    useImperativeHandle(ref, () => ({
-        toggleDrawerOutside() {
-            setIsOpen((prevState) => !prevState)
-        }
-    }));
+  const [isOpen, setIsOpen] = React.useState(false);
+  useImperativeHandle(ref, () => ({
+    toggleDrawerOutside() {
+      setIsOpen((prevState) => !prevState);
+    },
+  }));
 
-    const toggleDrawer = () => {
-        setIsOpen((prevState) => !prevState)
-    }
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
-    return (
-        <>
-            <div className='menu-student'>
-            <Drawer
-                open={isOpen}
-                onClose={toggleDrawer}
-                direction='left'
-                className='drawer'
-                size={500}
-                style={{ backgroundColor: "#0f3c4c", color: "white" }}
-            >
-                <div className='mt-5 drawerStudent'>
-                    <button onClick={toggleDrawer} className="btn btn-primary">Menu</button>
-                    <h2 onClick={() => { toggleDrawer(); props.onChoice("StudentProfile") }}>Mon profil</h2>
-                    <h2 onClick={() => { toggleDrawer(); props.onChoice("StudentTeam") }}>Mon équipe</h2>
-                    <h2 onClick={() => { toggleDrawer(); props.onChoice("StudentClass")}}>Ma classe</h2>
-                    <h2 onClick={() => { toggleDrawer(); props.onChoice("Shop") }}>Boutique</h2>
-                    <h2 onClick={() => { toggleDrawer(); props.onChoice("Quetes") }}>Quetes</h2>
-                    <h2 onClick={() => { toggleDrawer(); props.onChoice("Tutorial") }}>Tutoriel</h2>
-                    <h2 onClick={toggleDrawer}>Paramètres</h2>
-                </div>
-            </Drawer>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="left"
+        className="drawer"
+        size={300}
+        style={{ backgroundColor: "#ebbd3f", color: "white" }}
+      >
+        <div className="h-75 mt-5 drawerTeacher d-flex flex-column justify-content-between">
+          <motion.h2
+            whileHover={{
+              color: "#232826",
+              scale: 1.2,
+              originX: 0,
+              cursor: "pointer",
+            }}
+            whileTap={{ scale: 1.1 }}
+            onClick={() => {
+              toggleDrawer();
+              props.onChoice("StudentProfile");
+            }}
+          >
+            Mon profil
+          </motion.h2>
+          <motion.h2
+            whileHover={{
+              color: "#232826",
+              scale: 1.2,
+              originX: 0,
+              cursor: "pointer",
+            }}
+            whileTap={{ scale: 1.1 }}
+            onClick={() => {
+              toggleDrawer();
+              props.onChoice("StudentTeam");
+            }}
+          >
+            Mon équipe
+          </motion.h2>
+          <motion.h2
+            whileHover={{
+              color: "#232826",
+              scale: 1.2,
+              originX: 0,
+              cursor: "pointer",
+            }}
+            whileTap={{ scale: 1.1 }}
+            onClick={() => {
+              toggleDrawer();
+              props.onChoice("StudentClass");
+            }}
+          >
+            Ma classe
+          </motion.h2>
+          <motion.h2
+            whileHover={{
+              color: "#232826",
+              scale: 1.2,
+              originX: 0,
+              cursor: "pointer",
+            }}
+            whileTap={{ scale: 1.1 }}
+            onClick={() => {
+              toggleDrawer();
+              props.onChoice("Shop");
+            }}
+          >
+            Boutique
+          </motion.h2>
+          <motion.h2
+            whileHover={{
+              color: "#232826",
+              scale: 1.2,
+              originX: 0,
+              cursor: "pointer",
+            }}
+            whileTap={{ scale: 1.1 }}
+            onClick={() => {
+              toggleDrawer();
+              props.onChoice("Quetes");
+            }}
+          >
+            Quetes
+          </motion.h2>
+          <motion.h2
+            whileHover={{
+              color: "#232826",
+              scale: 1.2,
+              originX: 0,
+              cursor: "pointer",
+            }}
+            whileTap={{ scale: 1.1 }}
+            onClick={() => {
+              toggleDrawer();
+              props.onChoice("Tutorial");
+            }}
+          >
+            Tutoriel
+          </motion.h2>
+        </div>
+      </Drawer>
+    </>
+  );
 });
 
 export default StudentDrawer;
