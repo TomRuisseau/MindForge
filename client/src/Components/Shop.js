0b54d1569236c69e895a01c0d1ad97bfa983594b
AR5253 <113701450+AR5253@users.noreply.github.com>
2023-06-18 23:57:12 +0200

    studentclass.css,studentteam,tutoriel.css, studentquests.css

--- client/src/Components/Shop.js
+++ client/src/Components/Shop.js
@@ -126,14 +126,20 @@ function Shop(props) {
 
   return (
     <>
-      <h1 className="text-center text-center-orange">Boutique</h1>
-      <h2 className="text-center text-center-orange">
+               <br></br><br></br><br></br>
+               <div className ='  '>
+      <h1 className="text-center  text-center-color   ">Boutique</h1>
+      <h2 className="text-center  text-center-color">
         Vous avez {props.data[0].xp} xp
       </h2>
-      <div className="row1 row m-0">
-        <div className="col p-5">
-          <h2>Sorts</h2>
-          <div className="d-flex d-flex1 flex-row">
+      </div>
+      <br></br><br></br>
+      <br></br><br></br>
+      <br></br><br></br>
+      <div className="row1  row m-0">
+        <div className="col   p-5">
+          <h2 className="d-flex1">Sorts</h2>
+          <div className="d-flex flex-row">
             {spells.map((spell) => {
               let bg = spell.name === selected ? "selected" : "";
               let className = "m-2 " + bg; //mettre ici les autres classes de la div
@@ -156,9 +162,9 @@ function Shop(props) {
             })}
           </div>
         </div>
-        <div className="col p-5">
-          <h2>Skins</h2>
-          <div className=" d-flex d-flex1 flex-row">
+        <div className="col   p-5">
+          <h2 className="d-flex1">Skins</h2>
+          <div className=" d-flex  flex-row">
             {skins.map((skin) => {
               let bg = skin.name === selected ? "selected" : "";
               let className = "m-2 " + bg; //mettre ici les autres classes de la div
--- client/src/Components/StudentTeam.js
+++ client/src/Components/StudentTeam.js
@@ -41,16 +41,28 @@ function StudentTeam(props){
 
     return(
         <>
-            <div className='row row-team1 row-team'>
-                <div className='col-2 col-team'></div>
-                <h1 className='col col-team'>Mon équipe</h1>
+            <div className='row  row-team1 row-team'>
+                
+                <div className='col-2 col-21 col-team'></div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+               
+                <div className=' glass2 '>
+
+                <h1 className='col color: col-glass2 col-team'>Mon équipe</h1>
+               
                 <div className='row row-team'>
+              
                     <div className='col-2 col-team'></div>
+                   
                     {team.map((student) => {
                         return (
-                            
-                                
+                           
                                 <div className='col-2 col-team'>
+                                    
+
                                     
                                     <h3>{student.first_name}</h3>
                                     <h5>Vie:</h5><p>{student.hp}</p>
@@ -61,6 +73,7 @@ function StudentTeam(props){
                         )
                     }
                     )}
+                </div>
                 </div>
 
             </div>
