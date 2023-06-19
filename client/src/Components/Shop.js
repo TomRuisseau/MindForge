6c33dc147289f41ef08200de389ac9248e6df6f6
MarieDevulder <marie.devulder@student.junia.com>
2023-06-19 18:42:22 +0200

    fin boutique

--- client/src/Components/Shop.js
+++ client/src/Components/Shop.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import "../Styles/boutique.css";
+import "../Styles/Buttons.css";
+import "../Styles/Textes.css";
+import "../Styles/Glass.css";
+import { motion } from "framer-motion";
 
 function Shop(props) {
   const [spells, setSpells] = useState([]); // liste des sorts
   const [skins, setSkins] = useState([]); // liste des skins
   const [selected, setSelected] = useState(""); // liste des skins
   const [counter, setCounter] = useState(0); // pour forcer le rechargement de la page quand valide une quête
+  const [glassClass, setGlassClass] = useState(""); // classe du verre (tank, healer, mage)
 
   const isMountedRef = useRef(false);
 
+  let skinsMap = new Map();
+  skinsMap.set("tank", "Protecteur");
+  skinsMap.set("mage", "Mage");
+  skinsMap.set("healer", "Soigneur");
+
+  let spellsMap = new Map();
+  //tank
+  spellsMap.set("protection", "Protection");
+  spellsMap.set("halo salvateur", "Halo salvateur");
+  spellsMap.set("imposition des mains", "Imposition des mains");
+  spellsMap.set("purification", "Purification");
+  //healer
+  spellsMap.set("premiers soins", "Premiers soins");
+  spellsMap.set("apaisement majeur", "Apaisement majeur");
+  spellsMap.set("reviviscence", "Reviviscence");
+  spellsMap.set("soin de masse", "Soin de masse");
+  //mage
+  spellsMap.set("aura magique", "Aura magique");
+  spellsMap.set("vague de mana", "Vague de mana");
+  spellsMap.set("truquage du destin", "Truquage du destin");
+  spellsMap.set("expansion du savoir", "Expansion du savoir");
+
   useEffect(() => {
-    // axios.post("http://localhost:5000/getSpellsShop", { id: props.data[0].id, class: props.data[0].class })
-    //     .then((res) => {
-    //         setSpells(res.data);
-    //     }).catch((err) => {
-    //         console.log(err);
-    //     }
-    //     );
     if (!isMountedRef.current) {
       isMountedRef.current = true;
       axios
@@ -43,6 +62,16 @@ function Shop(props) {
       setSelected(e.target.getAttribute("data-key"));
     }
   };
+
+  //change class glass-tank or glass-healer or glass-mage
+  useEffect(() => {
+    const classToGlassClassMap = {
+      tank: "glass-tank",
+      mage: "glass-mage",
+      healer: "glass-healer",
+    };
+    setGlassClass(classToGlassClassMap[props.data[0].class]);
+  }, [props.data]);
 
   const buy = () => {
     let price = 0;
@@ -125,15 +154,20 @@ function Shop(props) {
   };
 
   return (
-    <>
-      <h1 className="text-center text-center-orange">Boutique</h1>
-      <h2 className="text-center text-center-orange">
-        Vous avez {props.data[0].xp} xp
-      </h2>
-      <div className="row1 row m-0">
-        <div className="col p-5">
-          <h2>Sorts</h2>
-          <div className="d-flex d-flex1 flex-row">
+    <div
+      className="w-100 d-flex flex-column align-items-center hug just-color-white justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="glass2 py-3 px-5 mb-5">
+        <h1 className="text-center just-color-yellow mb-4">Boutique</h1>
+        <h2 className="text-center just-color-white">
+          Vous avez {props.data[0].xp} XP
+        </h2>
+      </div>
+      <div className="h-75 d-flex flex-row justify-content-between mx-5">
+        <div className="box-size-2 glass1 d-flex flex-column justify-content-between p-4">
+          <h1 className="just-color-yellow">Sorts</h1>
+          <div className="h-100 w-75 d-flex flex-row justify-content-between align-self-center align-items-center flex-wrap">
             {spells.map((spell) => {
               let bg = spell.name === selected ? "selected" : "";
               let className = "m-2 " + bg; //mettre ici les autres classes de la div
@@ -141,24 +175,33 @@ function Shop(props) {
                 <div
                   key={spell.name}
                   data-key={spell.name}
-                  className={className}
+                  className="d-flex flex-column justify-content-center align-items-center h-50"
                   onClick={select}
+                  style={{ flexBasis: "50%" }}
                 >
-                  <h2>{spell.name.split("_").join(" ")}</h2>
-                  <img src={`media/spells/${spell.name}.webp`}></img>
+                  <p>{spellsMap.get(spell.name.split("_").join(" "))}</p>
                   <p>
-                    {!spell.owned
-                      ? "Prix : " + spell.cost + " XP"
-                      : "Déjà possédé"}
+                    {!spell.owned ? "Prix : " + spell.cost + " XP" : "Appris !"}
                   </p>
+                  <div
+                    data-key={spell.name}
+                    className={`${glassClass} rounded-circle`}
+                    style={{ width: "120px", height: "120px" }}
+                  >
+                    <img
+                      src={`media/spells/${spell.name}.webp`}
+                      className="rounded-circle"
+                      style={{ width: "100px", transform: "translate(9%,9%)" }}
+                    ></img>
+                  </div>
                 </div>
               );
             })}
           </div>
         </div>
-        <div className="col p-5">
-          <h2>Skins</h2>
-          <div className=" d-flex d-flex1 flex-row">
+        <div className="box-size-2 glass1 d-flex flex-column justify-content-between p-4">
+          <h1 className="just-color-yellow">Skins</h1>
+          <div className="d-flex d-flex1 flex-row">
             {skins.map((skin) => {
               let bg = skin.name === selected ? "selected" : "";
               let className = "m-2 " + bg; //mettre ici les autres classes de la div
@@ -166,19 +209,18 @@ function Shop(props) {
                 <div
                   key={skin.name}
                   data-key={skin.name}
-                  className={className}
+                  className="d-flex flex-column justify-content-between align-items-center"
                   onClick={select}
                 >
-                  <h2>{skin.name.split("_").join(" ")}</h2>
+                  <h2>{skinsMap.get(skin.name.split("_").join(" "))}</h2>
                   <img
                     src={`media/skin/${skin.name}.png`}
-                    style={{ width: "100%" }}
+                    style={{ width: "140%" }}
                   ></img>
                   <p>
                     {!skin.owned
                       ? "Prix : " + skin.cost + " XP"
-                      : "Déjà possédé" +
-                        (isEquiped(skin.name) ? " et équipé" : "")}
+                      : "En poche" + (isEquiped(skin.name) ? " et équipé" : "")}
                   </p>
                   <div />
                 </div>
@@ -201,7 +243,7 @@ function Shop(props) {
       ) : (
         <></>
       )}
-    </>
+    </div>
   );
 }
 
