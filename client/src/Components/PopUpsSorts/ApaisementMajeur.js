fad828799588bcd5b423d2a715e85f1ebdab5f67
MarieDevulder <marie.devulder@student.junia.com>
2023-06-21 17:12:30 +0200

    2 popups good

--- client/src/Components/PopUpsSorts/ApaisementMajeur.js
+++ client/src/Components/PopUpsSorts/ApaisementMajeur.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { motion } from "framer-motion";
+import "../../Styles/Textes.css";
+import "../../Styles/Glass.css";
+import "../../Styles/Buttons.css";
 
 function ApaisementMajeur(props) {
   const [students, setStudents] = useState([]);
@@ -40,46 +44,55 @@ function ApaisementMajeur(props) {
   };
 
   return (
-    <div className="px-3 py-3 w-50 h-50 bg-light position-absolute top-50 start-50 translate-middle text-center border rounded border-success d-flex flex-column align-items-center justify-content-between">
-      <div className="d-flex flex-row justify-content-between">
-        <h1 className="px-5">Apaisement majeur</h1>
-        <button className="btn-close h-auto" onClick={props.close}></button>
-      </div>
-      <div className="p-3 rounded">
-        <p style={{ fontSize: "22px" }}>
-          Tu soignes de <span className="text-danger">10 HP</span> un membre de
-          l'équipe ou toi-même.
-        </p>
-      </div>
-      <h3>Coût en mana : 6</h3>
-      <div className="w-50">
-        <form
-          onSubmit={useSpell}
-          className="d-flex flex-row justify-content-between"
-        >
-          <div className="d-flex flex-column">
-            <label htmlFor="text" className="mt-3">
-              Choisis un membre de l'équipe sur qui utiliser le sort :
-            </label>
-            <select
-              name="student"
-              id="student-select"
-              className="rounded"
-              onChange={(e) => setStudent(e.target.value)}
-            >
-              {students.map((eleve) => {
-                return (
-                  <option value={eleve.id} key={eleve.id}>
-                    {eleve.first_name} {eleve.surname}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <button type="submit" className="btn btn-success btn-lg">
-            Utiliser
-          </button>
-        </form>
+    <div
+      className="classic-glass-moins-flou hug just-color-white position-absolute top-0 start-0 translate-middl w-100 h-100 d-flex align-items-center justify-content-center"
+      style={{ zIndex: 2 }}
+    >
+      <div className="px-3 py-3 w-50 h-50 glass3 text-center d-flex flex-column align-items-center justify-content-between">
+        <div className="d-flex flex-row justify-content-between">
+          <h1 className="px-5">Apaisement majeur</h1>
+          <motion.button
+            whileHover={{ scale: 2 }}
+            className="btn-close btn-close-white m-3 position-absolute top-0 end-0"
+            onClick={props.close}
+          ></motion.button>
+        </div>
+        <div className="p-3 rounded">
+          <p style={{ fontSize: "22px" }}>
+            Tu soignes de <span className="text-danger">10 HP</span> un membre
+            de l'équipe ou toi-même.
+          </p>
+        </div>
+        <h3>Coût en mana : 6</h3>
+        <div className="w-50">
+          <form
+            onSubmit={useSpell}
+            className="d-flex flex-row justify-content-between"
+          >
+            <div className="d-flex flex-column">
+              <label htmlFor="text" className="mt-3">
+                Choisis un membre de l'équipe sur qui utiliser le sort :
+              </label>
+              <select
+                name="student"
+                id="student-select"
+                className="rounded"
+                onChange={(e) => setStudent(e.target.value)}
+              >
+                {students.map((eleve) => {
+                  return (
+                    <option value={eleve.id} key={eleve.id}>
+                      {eleve.first_name} {eleve.surname}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <button type="submit" className="btn btn-success btn-lg">
+              Utiliser
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- client/src/Components/PopUpsSorts/AuraMagique.js
+++ client/src/Components/PopUpsSorts/AuraMagique.js
@@ -1,5 +1,9 @@
 import React from "react";
 import axios from "axios";
+import { motion } from "framer-motion";
+import "../../Styles/Glass.css";
+import "../../Styles/Textes.css";
+import "../../Styles/Buttons.css";
 
 function AuraMagique(props) {
   const useSpell = () => {
@@ -21,22 +25,31 @@ function AuraMagique(props) {
   };
 
   return (
-    <div className="px-3 py-3 w-50 h-50 bg-light position-absolute top-50 start-50 translate-middle text-center border rounded border-success d-flex flex-column align-items-center justify-content-between">
-      <div className="d-flex flex-row justify-content-between">
-        <h1 className="px-5">Aura Magique</h1>
-        <button className="btn-close h-auto" onClick={props.close}></button>
-      </div>
-      <div className="p-3 rounded">
-        <p style={{ fontSize: "22px" }}>
-          Tu restitues <span className="text-danger">2 de mana</span> à tous les
-          autres membres de ton équipe.
-        </p>
-      </div>
-      <h3>Coût en mana : 4</h3>
-      <div>
-        <button onClick={useSpell} className="btn btn-success btn-lg">
-          Utiliser
-        </button>
+    <div
+      className="classic-glass-moins-flou hug just-color-white position-absolute top-0 start-0 translate-middl w-100 h-100 d-flex align-items-center justify-content-center"
+      style={{ zIndex: 2 }}
+    >
+      <div className="px-3 py-3 w-50 h-50 glass3 text-center d-flex flex-column align-items-center justify-content-between">
+        <div className="d-flex flex-row justify-content-between">
+          <h1 className="px-5">Apaisement majeur</h1>
+          <motion.button
+            whileHover={{ scale: 2 }}
+            className="btn-close btn-close-white m-3 position-absolute top-0 end-0"
+            onClick={props.close}
+          ></motion.button>
+        </div>
+        <div className="p-3 rounded">
+          <p style={{ fontSize: "22px" }}>
+            Tu restitues <span className="text-danger">2 de mana</span> à tous
+            les autres membres de ton équipe.
+          </p>
+        </div>
+        <h3>Coût en mana : 4</h3>
+        <div>
+          <button onClick={useSpell} className="btn btn-success btn-lg">
+            Utiliser
+          </button>
+        </div>
       </div>
     </div>
   );
