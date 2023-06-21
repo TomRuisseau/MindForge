4a930c17ffba355879ff2fb195984832ed6c7a16
TomRuisseau <tom.ruisseau@student.junia.com>
2023-06-21 15:29:20 +0200

    Revert "Maj CSS couleur du texte noir"
    This reverts commit 537e5265c3f0c7a23463d21e74d6537a9fb09638.

--- client/src/Components/PopUpsSorts/VagueDeMana.js
+++ client/src/Components/PopUpsSorts/VagueDeMana.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./text.css";
 
 function VagueDeMana(props) {
   const [students, setStudents] = useState([]);
