91733156d92c6472b77ed8aa56d096d8f1af3ae6
MarieDevulder <marie.devulder@student.junia.com>
2023-06-22 11:41:31 +0200

    popups mage presque ok

--- client/src/Components/PopUpsSorts/TruquageDuDestin.js
+++ client/src/Components/PopUpsSorts/TruquageDuDestin.js
@@ -1,19 +1,32 @@
+import React from "react";
+import { motion } from "framer-motion";
+
 function TruquageDuDestin(props) {
   return (
-    <div className="px-3 py-3 w-50 h-50 bg-light position-absolute top-50 start-50 translate-middle text-center border rounded border-success d-flex flex-column align-items-center justify-content-between">
-      <div className="d-flex flex-row justify-content-between">
-        <h1 className="px-5">Truquage du destin</h1>
-        <button className="btn-close h-auto" onClick={props.close}></button>
+    <div
+      className="classic-glass-moins-flou hug just-color-white position-absolute top-0 start-0 translate-middl w-100 h-100 d-flex align-items-center justify-content-center"
+      style={{ zIndex: 2 }}
+    >
+      <div className="px-3 py-3 w-50 h-50 glass3 text-center d-flex flex-column align-items-center justify-content-between">
+        <div className="d-flex flex-row justify-content-between">
+          <h1 className="px-5">Aura magique</h1>
+          <motion.button
+            whileHover={{ scale: 2 }}
+            className="btn-close btn-close-white m-3 position-absolute top-0 end-0"
+            onClick={props.close}
+          ></motion.button>
+        </div>
+        <div className="p-3 rounded">
+          <p style={{ fontSize: "22px" }}>
+            Permet de généraliser un gain d'XP individuel à toute l'équipe.
+          </p>
+          <p style={{ fontSize: "22px" }}>
+            (Est activé par le professeur pendant l'attribution d'une
+            récompense)
+          </p>
+        </div>
+        <h3>Coût en mana : 5</h3>
       </div>
-      <div className="p-3 rounded">
-        <p style={{ fontSize: "22px" }}>
-          Permet de généraliser un gain d'XP individuel à toute l'équipe.
-        </p>
-        <p style={{ fontSize: "22px" }}>
-          (Est activé par le professeur pendant l'attribution d'une récompense)
-        </p>
-      </div>
-      <h3>Coût en mana : 5</h3>
     </div>
   );
 }
--- client/src/Components/PopUpsSorts/VagueDeMana.js
+++ client/src/Components/PopUpsSorts/VagueDeMana.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { motion } from "framer-motion";
+import "../../Styles/Glass.css";
+import "../../Styles/Textes.css";
 
 function VagueDeMana(props) {
   const [students, setStudents] = useState([]);
@@ -42,46 +45,55 @@ function VagueDeMana(props) {
   };
 
   return (
-    <div className="px-3 py-3 w-50 h-50 bg-light position-absolute top-50 start-50 translate-middle text-center border rounded border-success d-flex flex-column align-items-center justify-content-between">
-      <div className="d-flex flex-row justify-content-between">
-        <h1 className="px-5">Vague de mana</h1>
-        <button className="btn-close h-auto" onClick={props.close}></button>
-      </div>
-      <div className="p-3 rounded">
-        <p style={{ fontSize: "22px" }}>
-          Tu restaures <span className="text-danger">tout le mana</span> d'un
-          membre de ton équipe (sauf toi) en fonction de sa classe.
-        </p>
-      </div>
-      <h3>Coût en mana : 6</h3>
-      <div className="w-50">
-        <form
-          onSubmit={useSpell}
-          className="d-flex flex-row justify-content-between"
-        >
-          <div className="d-flex flex-column">
-            <label htmlFor="text" className="mt-3">
-              Choisis un membre de l'équipe sur qui utiliser le sort :
-            </label>
-            <select
-              name="student"
-              id="student-select"
-              className="rounded"
-              onChange={(e) => setStudent(e.target.value)}
-            >
-              {students.map((eleve) => {
-                return (
-                  <option value={eleve.id} key={eleve.id}>
-                    {eleve.first_name} {eleve.surname}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <button type="submit" className="btn btn-success btn-lg">
-            Utiliser
-          </button>
-        </form>
+    <div
+      className="classic-glass-moins-flou hug just-color-white position-absolute top-0 start-0 translate-middl w-100 h-100 d-flex align-items-center justify-content-center"
+      style={{ zIndex: 2 }}
+    >
+      <div className="px-3 py-3 w-50 h-50 glass3 text-center d-flex flex-column align-items-center justify-content-between">
+        <div className="d-flex flex-row justify-content-between">
+          <h1 className="px-5">Vague de mana</h1>
+          <motion.button
+            whileHover={{ scale: 2 }}
+            className="btn-close btn-close-white m-3 position-absolute top-0 end-0"
+            onClick={props.close}
+          ></motion.button>
+        </div>
+        <div className="p-3 rounded">
+          <p style={{ fontSize: "22px" }}>
+            Tu restaures <span className="text-danger">tout le mana</span> d'un
+            membre de ton équipe (sauf toi) en fonction de sa classe.
+          </p>
+        </div>
+        <h3>Coût en mana : 6</h3>
+        <div className="w-50">
+          <form
+            onSubmit={useSpell}
+            className="d-flex flex-row justify-content-between"
+          >
+            <div className="d-flex flex-column">
+              <label htmlFor="text" className="mt-3">
+                Choisis un membre de l'équipe sur qui utiliser le sort :
+              </label>
+              <select
+                name="student"
+                id="student-select"
+                className="rounded"
+                onChange={(e) => setStudent(e.target.value)}
+              >
+                {students.map((eleve) => {
+                  return (
+                    <option value={eleve.id} key={eleve.id}>
+                      {eleve.first_name} {eleve.surname}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <button type="submit" className="btn btn-success btn-lg">
+              Utiliser
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
